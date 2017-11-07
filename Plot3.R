b569176ed51021dd3e3f0d9236aa9ab343c168d7
dbridgham <31636387+DonnaBridgham@users.noreply.github.com>
2017-11-06 19:58:29 -0500

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -46,6 +46,8 @@ legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,
 
 ## Saving to file
 
-dev.copy(png, file="plot3.png", height=480, width=480)
+dev.copy(png, file="plot3.png")
+
+##Close
 
 dev.off()
