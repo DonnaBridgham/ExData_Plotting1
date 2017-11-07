eac3b1527aa3dee8b50cdc660df1dbe443a200c9
dbridgham <31636387+DonnaBridgham@users.noreply.github.com>
2017-11-06 19:55:38 -0500

    change data

--- Plot1.R
+++ Plot1.R
@@ -13,19 +13,19 @@ data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 
 rm(data_full)
 
-##DataTime Function
+## Convert Time
 
 datetime <- paste(as.Date(data$Date), data$Time)
 
 data$Datetime <- as.POSIXct(datetime)
 
 
-##Histogram
+## Create Histogram
 hist(data$Global_active_power, main="Global Active Power", 
 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
-dev.copy(png, file="Plot1.png", height=480, width=480)
+dev.copy(png, file="Plot1.png")
 
 ##Close
 
